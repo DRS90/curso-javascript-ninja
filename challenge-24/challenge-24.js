d10ad027862ac1800c26d67cee59e14dc2f87a73
Douglas <drs.90@hotmail.com>
2018-03-13 12:43:09 -0300

    challenge-24

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -1,75 +1,98 @@
-/*
-Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
-o código, conforme vimos na aula anterior. Quebrar as responsabilidades
-em funções, onde cada função faça somente uma única coisa, e faça bem feito.
-
-- Remova as duplicações de código;
-- agrupe os códigos que estão soltos em funções (declarações de variáveis,
-listeners de eventos, etc);
-- faça refactories para melhorar esse código, mas de forma que o mantenha com a
-mesma funcionalidade.
-*/
-
-var $visor = document.querySelector('[data-js="visor"]');
-var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
-var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
-var $buttonCE = document.querySelector('[data-js="button-ce"]');
-var $buttonEqual = document.querySelector('[data-js="button-equal"]');
-
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
-  button.addEventListener('click', handleClickNumber, false);
-});
-Array.prototype.forEach.call($buttonsOperations, function(button) {
-  button.addEventListener('click', handleClickOperation, false);
-});
-$buttonCE.addEventListener('click', handleClickCE, false);
-$buttonEqual.addEventListener('click', handleClickEqual, false);
-
-function handleClickNumber() {
-  $visor.value += this.value;
-}
-
-function handleClickOperation() {
-  $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  $visor.value += this.value;
-}
-
-function handleClickCE() {
-  $visor.value = 0;
-}
-
-function isLastItemAnOperation(number) {
-  var operations = ['+', '-', 'x', '÷'];
-  var lastItem = number.split('').pop();
-  return operations.some(function(operator) {
-    return operator === lastItem;
-  });
-}
-
-function removeLastItemIfItIsAnOperator(number) {
-  if(isLastItemAnOperation(number)) {
-    return number.slice(0, -1);
+(function (window, document) {
+  'use strict'
+  /*
+  Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
+  o código, conforme vimos na aula anterior. Quebrar as responsabilidades
+  em funções, onde cada função faça somente uma única coisa, e faça bem feito.
+  
+  - Remova as duplicações de código;
+  - agrupe os códigos que estão soltos em funções (declarações de variáveis,
+  listeners de eventos, etc);
+  - faça refactories para melhorar esse código, mas de forma que o mantenha com a
+  mesma funcionalidade.
+  */
+ var $visor = document.querySelector('[data-js="visor"]');
+ var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
+ var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
+ var $buttonCE = document.querySelector('[data-js="button-ce"]');
+ var $buttonEqual = document.querySelector('[data-js="button-equal"]');
+ 
+ function initialize() {
+   initEvents()
+ }
+ 
+  function initEvents() {
+    Array.prototype.forEach.call($buttonsNumbers, function (button) {
+      button.addEventListener('click', handleClickNumber, false);
+    });
+    Array.prototype.forEach.call($buttonsOperations, function (button) {
+      button.addEventListener('click', handleClickOperation, false);
+    });
+    $buttonCE.addEventListener('click', handleClickCE, false);
+    $buttonEqual.addEventListener('click', handleClickEqual, false);
   }
-  return number;
-}
 
-function handleClickEqual() {
-  $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-  $visor.value = allValues.reduce(function(accumulated, actual) {
+  function handleClickNumber() {
+    $visor.value += this.value;
+  }
+
+  function handleClickOperation() {
+    $visor.value = removeLastItemIfItIsAnOperator($visor.value);
+    $visor.value += this.value;
+  }
+
+  function handleClickCE() {
+    $visor.value = 0;
+  }
+
+  function isLastItemAnOperation(number) {
+    var operations = getOperations();
+    var lastItem = number.split('').pop();
+    return operations.some(function (operator) {
+      return operator === lastItem;
+    });
+  }
+  function getOperations(){
+    return Array.prototype.map.call($buttonsOperations, button => button.value)
+  }
+
+  function removeLastItemIfItIsAnOperator(string) {
+    if (isLastItemAnOperation(string)) {
+      return string.slice(0, -1);
+    }
+    return string;
+  }
+
+  function handleClickEqual() {
+    $visor.value = removeLastItemIfItIsAnOperator($visor.value);
+    var allValues = $visor.value.match(getRegexOperations());
+    $visor.value = allValues.reduce(calculateAllValues)
+  }
+
+  function getRegexOperations(){
+    return new RegExp('\\d+[' + getOperations().join('') + ']?', 'g')
+  }
+
+  function calculate(a, b, operator) {
+    const operation = {
+      '+': () => +a + +b,
+      '-': () => +a - +b,
+      'x': () => +a * +b,
+      '÷': () => +a / +b,
+    }
+    return operation[operator](a, b)
+  }
+
+  function calculateAllValues(accumulated, actual) {
     var firstValue = accumulated.slice(0, -1);
     var operator = accumulated.split('').pop();
     var lastValue = removeLastItemIfItIsAnOperator(actual);
-    var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-    switch(operator) {
-      case '+':
-        return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-      case '-':
-        return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-      case 'x':
-        return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-      case '÷':
-        return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
-    }
-  });
-}
+    var lastOperator = getLastOperator(actual)
+    return calculate(firstValue, lastValue, operator) + lastOperator
+  };
+
+  function getLastOperator(value) {
+    return isLastItemAnOperation(value) ? value.split('').pop() : '';
+  }
+  initialize() // como tudo foi criado em função é preciso inicializar de algum modo
+})(window, document)
